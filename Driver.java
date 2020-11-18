3c394439cfbcf71b892408e6fda549aac9988ad9
Sleepo132 <74639890+Sleepo132@users.noreply.github.com>
2020-11-18 08:58:31 -0500

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,4 +1,7 @@
 import javax.swing.*;
+
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -15,31 +18,70 @@ public class Driver extends JFrame implements ActionListener {
 
 	public static void main(String[] args) {
 		new Driver();
-
 	}
 
 	public Driver() {
-
-		movie = new JButton("Movie");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(800, 800);
+		panel.setLayout(new GridBagLayout());
+		movie = new JButton("Movie"); // Initializes
 		show = new JButton("Show");
 		anime = new JButton("Anime");
 		manga = new JButton("Manga");
 		message = new JLabel("Choose what you would like to log.");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(1368, 768);
-		movie.addActionListener(this);
+		check = 6;
+		
+		movie.addActionListener(this); // Adds listener to movie
 		show.addActionListener(this);
 		anime.addActionListener(this);
 		manga.addActionListener(this);
+		
+		
+		panel.add(message, new GridBagConstraints());
+		panel.add(anime, new GridBagConstraints());
+		panel.add(manga, new GridBagConstraints());
+		panel.add(show, new GridBagConstraints());
+		panel.add(movie, new GridBagConstraints());
 
-		panel.add(message);
-		panel.add(anime);
-		panel.add(manga);
-		panel.add(show);
-		panel.add(movie);
-		add(panel);
-		setVisible(true);
-		setResizable(false);
+		add(panel); // Adds the panel
+		setVisible(true); // Lets person see panel
+		setResizable(false); // Cannot resize window
+
+		if (checkType().equals("Movie") ) {
+			clearScreen();
+		}
+	}
+	
+	public JButton buttonGetter(JButton value) {
+		return value;
+	}
+
+	public byte getCheck() {
+		return check;
+	}
+	// Checks what has been clicked by user
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == movie) {
+			check = 1;
+		} else if (e.getSource() == show) {
+			check = 2;
+		} else if (e.getSource() == anime) {
+			check = 3;
+		} else if (e.getSource() == manga) {
+			check = 4;
+		}
+	}
+
+	/* Method is used to clear screen after a choice has been made, allowing the
+	 * user to enter the name of the show, movie, etc..
+	 */
+	public void clearScreen() {
+		panel.removeAll();
+		revalidate();
+		repaint();
+	}
+
+	public String checkType() {
 
 		switch (check) {
 		case 0:
@@ -59,32 +101,8 @@ public class Driver extends JFrame implements ActionListener {
 			clearScreen();
 			break;
 		}
-	}
 
-	public JButton buttonGetter(JButton value) {
-		return value;
-	}
-
-	public byte getCheck() {
-		return check;
-	}
-
-	public void actionPerformed(ActionEvent e) {
-		if (e.getSource() == movie) {
-			check = 0;
-		} else if (e.getSource() == show) {
-			check = 1;
-		} else if (e.getSource() == anime) {
-			check = 2;
-		} else if (e.getSource() == manga) {
-			check = 3;
-		}
-	}
-
-	public void clearScreen() {
-		panel.removeAll();
-		revalidate();
-		repaint();
+		return type;
 	}
 
 }
