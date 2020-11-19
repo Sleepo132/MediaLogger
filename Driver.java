9f76245024fefbcccdf3d1220c8887d254744b73
Sleepo132 <74639890+Sleepo132@users.noreply.github.com>
2020-11-18 19:46:12 -0500

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,26 +1,28 @@
 import javax.swing.*;
 
+import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Driver extends JFrame implements ActionListener {
 
-	private JButton movie;
-	private JButton show;
-	private JButton anime;
-	private JButton manga;
+	private JButton movie, show, anime, manga;
 	private JLabel message;
+	private JTextField movieText, showText, animeText, mangaText;
 	private static JPanel panel = new JPanel();
-	private byte check;
+	private String movieName;
 	private String type;
+	private JPanel panel2 = new JPanel();
 
 	public static void main(String[] args) {
 		new Driver();
 	}
 
 	public Driver() {
+
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(800, 800);
 		panel.setLayout(new GridBagLayout());
@@ -29,47 +31,58 @@ public class Driver extends JFrame implements ActionListener {
 		anime = new JButton("Anime");
 		manga = new JButton("Manga");
 		message = new JLabel("Choose what you would like to log.");
-		check = 10;
+		movieText = new JTextField();
+		showText = new JTextField();
+		animeText = new JTextField();
+		mangaText = new JTextField();
+
+		movie.setFocusable(false);
+		show.setFocusable(false);
+		manga.setFocusable(false);
+		anime.setFocusable(false);
 
 		movie.addActionListener(this); // Adds listener to movie
 		show.addActionListener(this);
 		anime.addActionListener(this);
 		manga.addActionListener(this);
 
-		if (check == 1) {
-			clearScreen();
-		}
+		message.setHorizontalTextPosition(message.CENTER);
+		message.setVerticalTextPosition(message.CENTER);
 
+		panel2.setLayout(new GridLayout(1, 4));
+		panel2.setPreferredSize(new Dimension(400, 30));
 		panel.add(message, new GridBagConstraints());
-		panel.add(anime, new GridBagConstraints());
-		panel.add(manga, new GridBagConstraints());
-		panel.add(show, new GridBagConstraints());
-		panel.add(movie, new GridBagConstraints());
+		panel2.add(anime, new GridBagConstraints());
+		panel2.add(manga, new GridBagConstraints());
+		panel2.add(show, new GridBagConstraints());
+		panel2.add(movie, new GridBagConstraints());
 
+		panel.add(panel2);
 		add(panel); // Adds the panel
 		setVisible(true); // Lets person see panel
 		setResizable(false); // Cannot resize window
-
+		System.out.println(movieName);
 	}
 
 	public JButton buttonGetter(JButton value) {
 		return value;
 	}
 
-	public byte getCheck() {
-		return check;
-	}
-
 	// Checks what has been clicked by user
 	public void actionPerformed(ActionEvent e) {
 		if (e.getSource() == movie) {
-			check = 1;
+			type = "Movie";
+			clearScreen();
+			afterButton();
 		} else if (e.getSource() == show) {
-			check = 2;
+			type = "Show";
+			clearScreen();
 		} else if (e.getSource() == anime) {
-			check = 3;
+			type = "Anime";
+			clearScreen();
 		} else if (e.getSource() == manga) {
-			check = 4;
+			type = "Manga";
+			clearScreen();
 		}
 	}
 
@@ -83,32 +96,19 @@ public class Driver extends JFrame implements ActionListener {
 		panel.repaint();
 	}
 
-	public String checkType() {
-
-		switch (check) {
-		case 0:
-			type = "Movie";
-			panel.removeAll();
-			break;
-		case 1:
-			type = "Show";
-			panel.removeAll();
-			break;
-		case 2:
-			type = "Anime";
-			panel.removeAll();
-			break;
-		case 3:
-			type = "Manga";
-			panel.removeAll();
-			break;
-		default:
-			type = "none";
-			panel.removeAll();
-			break;
+	public void afterButton() {
+		if (type.equals("Movie")) {
+			panel.add(new JLabel("Which movie would you like to log?"));
+			movieText.addActionListener(this);
+			movieText.setPreferredSize(new Dimension(100, 25));
+			panel.add(movieText);
+			movieName = movieText.getText();
+		} else if (type.equals("Manga")) {
+			panel.add(new JLabel("Which manga would you like to log?"));
+		} else if (type.equals("Anime")) {
+			panel.add(new JLabel("Which anime would you like to log?"));
+		} else if (type.equals("Show")) {
+			panel.add(new JLabel("Which show would you like to log?"));
 		}
-
-		return type;
 	}
-
 }
