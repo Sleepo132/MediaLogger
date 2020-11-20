8b0f0fc38f9cd695fa6f8c1fedfb8eed689f2366
Sleepo132 <74639890+Sleepo132@users.noreply.github.com>
2020-11-20 14:19:03 -0500

    Update WriteToFile.java

--- WriteToFile.java
+++ WriteToFile.java
@@ -2,17 +2,20 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
-public class WriteToFile {
-
-	public static void main(String[] args) {
-		File textLog = new File("textLog.txt");
+public class WriteToFile extends Driver {
+	
+	public static void writeToFile(String text) {
 		try {
-			FileWriter writer = new FileWriter(textLog);
+			File file = new File("file.txt");
+			FileWriter fw = new FileWriter(file, true);
+			fw.write(text);
+			fw.close();
+			System.out.println("Successfully wrote to the file.");
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
+		
 	}
 
 }
