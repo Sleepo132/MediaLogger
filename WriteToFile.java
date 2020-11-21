f31135cb994d37e1d57b27352a947538aeac1e93
Sleepo132 <74639890+Sleepo132@users.noreply.github.com>
2020-11-20 23:50:22 -0500

    Update WriteToFile.java

--- WriteToFile.java
+++ WriteToFile.java
@@ -4,11 +4,19 @@ import java.io.IOException;
 
 public class WriteToFile extends Driver {
 	
-	public static void writeToFile(String text) {
+	public static void writeToFile() {
 		try {
+			
 			File file = new File("file.txt");
 			FileWriter fw = new FileWriter(file, true);
-			fw.write(text);
+			if (type.equals("Movie")) {
+				fw.write(movieName + "\n");
+			} else if (type.equals("Show") || type.equals("Anime")) {
+				fw.write(movieName + " Episode: " + showNum + "\n");
+			} else if (type.equals("Manga")) {
+				fw.write(movieName + " Chapter: " + showNum + "\n");
+			}
+			
 			fw.close();
 			System.out.println("Successfully wrote to the file.");
 		} catch (IOException e) {
